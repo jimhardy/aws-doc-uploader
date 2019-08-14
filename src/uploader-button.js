2b877dc53fa226871098957349f72bcf55507e26
James Hardy <jamesthardy@gmail.com>
2019-08-14 13:15:05 +0100

    formatting

--- src/uploader-button.js
+++ src/uploader-button.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileUpload } from '@fortawesome/free-solid-svg-icons'
 
 export default props => (
   <div className="buttons fadein">
     <div className="button">
       <label htmlFor="multi">
-        <FontAwesomeIcon icon={faFileUpload} color="#6d84b4" size="10x" />
+        <FontAwesomeIcon icon={faFileUpload} color="#6d84b4" size="11x" />
       </label>
       <input type="file" id="multi" onChange={props.onChange} multiple />
     </div>
   </div>
-);
+)
